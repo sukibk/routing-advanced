6e0673c8ecbe27dc1257be71510a1ddcac342552
Marko Sudar <mjsudar21@catawba.edu>
2023-03-13 00:52:03 -0400

    //

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,29 +1,34 @@
-import { useLoaderData, json } from 'react-router-dom';
+import { useLoaderData, json, defer, Await } from 'react-router-dom';
+import { Suspense } from 'react';
 
 import EventsList from '../components/EventsList';
 
 export const EventsPage = () => {
-    const data = useLoaderData();
-    const events = data.events;
-
-    // if(data.isError){
-    //     return <p>{data.message}</p>
-    // }
+    const {events} = useLoaderData();
 
     return (
-        <>
-            <EventsList events={events} />
-        </>
-    );
+    <Suspense fallback={<p style={{textAlign: 'center'}}>Loading...</p>}>
+    <Await resolve={events}>
+        {(loadedEvents) => <EventsList events={loadedEvents} />}
+    </Await>
+    </Suspense>)
 
 }
 
-export async function loader(){
+async function loadEvents(){
     const response = await fetch('http://localhost:8080/events');
     if (!response.ok) {
         // return {isError: true, message: 'Could not fetch events.'}
-        throw new json({message: 'Couldn\'t fetch events'}, {status: 500})
+        throw json({message: 'Couldn\'t fetch events'}, {status: 500})
     } else {
-        return response;
+       // return response;
+        const resData = await response.json();
+        return resData.events;
     }
+}
+
+export function loader(){
+    return defer({
+        events: loadEvents()
+    })
 }
--- frontend/src/pages/NewEvent.js
+++ frontend/src/pages/NewEvent.js
@@ -2,36 +2,36 @@ import EventForm from "../components/EventForm";
 import {json, redirect} from 'react-router-dom'
 
 export const NewEventPage = () => {
-    return <EventForm />
+    return <EventForm method='post'/>
 }
 
-export async function action({request, params}) {
-    const data = await request.formData();
-
-    const eventData = {
-        title: data.get('title'),
-        image: data.get('image'),
-        date: data.get('date'),
-        description: data.get('description')
-    }
-
-    const response = await fetch ('http://localhost:8080/events',
-        {
-            method: 'POST',
-            body: JSON.stringify(eventData),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-
-    if(response.status === 422) //response code set in backend in case of validation error
-    {
-        return response;
-    }
-
-    if(!response.ok) {
-        throw json ({message: 'Couldn\'t save the event'}, {status: 500})
-    }
-
-    return redirect('/events')
-}
+// export async function action({request, params}) {
+//     const data = await request.formData();
+//
+//     const eventData = {
+//         title: data.get('title'),
+//         image: data.get('image'),
+//         date: data.get('date'),
+//         description: data.get('description')
+//     }
+//
+//     const response = await fetch ('http://localhost:8080/events',
+//         {
+//             method: 'POST',
+//             body: JSON.stringify(eventData),
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             }
+//         })
+//
+//     if(response.status === 422) //response code set in backend in case of validation error
+//     {
+//         return response;
+//     }
+//
+//     if(!response.ok) {
+//         throw json ({message: 'Couldn\'t save the event'}, {status: 500})
+//     }
+//
+//     return redirect('/events')
+// }
